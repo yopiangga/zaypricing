d5629bd5346479e058a227f0e4499b356ae0d6b5
@yopigambyok <yopigambyok@gmail.com>
2021-02-10 09:42:09 +0700

    pacet title

---
+++ src/Pages/BundlePricing.js
@@ -0,0 +1,52 @@
+
+
+
+function BundlePricing() {
+    
+    return(
+        <div className="BundlePricing-Page">
+            <div className="content">
+                <div className="content-head">
+                    <h1>Choose products for your bundle.</h1>
+                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Dicta aut quisquam, accusamus exercitationem.</p>
+                </div>
+                <div className="content-body">
+                    <div className="content-choose">
+
+                        <div className="packet">
+                            <div className="packet-title">
+                                <div className="title-heading">
+                                    <h2>Annual Pricing</h2>
+                                    <a href="">See all features</a>
+                                </div>
+                                <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit.</p>
+                            </div>
+                            <div className="packet-body">
+                                <div className="card-group">
+                                    <div className="card">
+                                        <h4>Free</h4>
+                                        <h5><span>$0,00</span>/year</h5>
+                                    </div>
+                                    <div className="card">
+                                        <h4>Advanced</h4>
+                                        <h5><span>$0,00</span>/year</h5>
+                                    </div>
+                                    <div className="card">
+                                        <h4>Ultra</h4>
+                                        <h5><span>$99,00</span>/year</h5>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+
+                    </div>
+                    <div className="content-price">
+
+                    </div>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default BundlePricing
